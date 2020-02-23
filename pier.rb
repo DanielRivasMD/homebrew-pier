b8081c642616a77c70e30c12d20d25fa9790b742
DanielRivasMD <danielrivasmd@gmail.com>
2020-02-23 14:19:10 +0100

    update format

--- pier.rb
+++ pier.rb
@@ -1,8 +1,8 @@
 class Pier < Formula
   desc "A Linux script management CLI written in Rust"
   homepage ""
-  url "https://github.com/DanielRivasMD/pier/releases/download/v0.2.1/pier.tar.gz"
-  sha256 "c97eaecbb2874deb85bcc8a4b03ccb46a5b20b213fd139d2fc3269ca1fcba914"
+  url "https://github.com/DanielRivasMD/pier/releases/download/v0.2.2/pier.tar.gz"
+  sha256 "c861f72130b17934f7ee5f3eb95db587e9128eadfbcb404902f7adb63a57519e"
 
   def install
     bin.install "pier"
