f5adb8db92585a8058a760008c652c9adb705c8e
DanielRivasMD <danielrivasmd@gmail.com>
2020-07-12 10:09:25 +0200

    merge latest 0.1.4

--- pier.rb
+++ pier.rb
@@ -1,8 +1,9 @@
 class Pier < Formula
   desc "A Linux script management CLI written in Rust"
   homepage ""
-  url "https://github.com/DanielRivasMD/pier/releases/download/v0.2.2/pier.tar.gz"
-  sha256 "c861f72130b17934f7ee5f3eb95db587e9128eadfbcb404902f7adb63a57519e"
+  url "https://github.com/DanielRivasMD/pier/releases/latest/download/pier.tar.gz"
+  sha256 "e02a429fd112120df75e61fbde8a2d048f6f8aa56d5d3e54e60637835e0bf3f9"
+  version "0.2.3"
 
   def install
     bin.install "pier"
