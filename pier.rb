3beeccf0deeaf4f458658bd32c2c9101117c2f88
DanielRivasMD <danielrivasmd@gmail.com>
2020-07-12 10:30:30 +0200

    add homepage & latest

--- pier.rb
+++ pier.rb
@@ -1,9 +1,10 @@
 class Pier < Formula
   desc "A Linux script management CLI written in Rust"
-  homepage ""
+  version '0.2.3'
+  homepage "https://github.com/DanielRivasMD/pier"
+
   url "https://github.com/DanielRivasMD/pier/releases/latest/download/pier.tar.gz"
   sha256 "e02a429fd112120df75e61fbde8a2d048f6f8aa56d5d3e54e60637835e0bf3f9"
-  version "0.2.3"
 
   def install
     bin.install "pier"
